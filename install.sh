7b520b70c5fa343c851d3b1804e36dd95959dcc5
Votre Nom <Vous@exemple.com>
2020-03-04 15:58:41 +0100

    install v3

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-######	Install Dependency	######
+########	Install Dependency	########
 echo -ne '|#                   |(0%)\r'
 sleep 1 
 echo -n $(sudo apt update > /dev/null 2>&1)
@@ -14,7 +14,8 @@ echo -n $(sudo apt install gnupg-agent > /dev/null 2>&1)
 echo -n $(sudo apt install software-properties-common > /dev/null 2>&1)
 echo -ne '|######              |(30%)\r'
 sleep 1
-######	Install Docker		######
+
+########	Install Docker		########
 echo -n $(curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - > /dev/null 2>&1)
 echo -n $(sudo apt-key fingerprint 0EBFCD88 > /dev/null 2>&1)
 echo -ne '|########            |(40%)\r'
@@ -29,17 +30,22 @@ sleep 1
 echo -n $(sudo docker run hello-world > /dev/null 2>&1)
 echo -ne '|############        |(60%)\r'
 sleep 1
-###### Test de version Docker	######
+
+########   Test de version Docker	########
 echo -n $(sudo docker --version)
 echo -ne '|##############      |(70%)\r'
-sleep 1 
-######	Install Docker-Compose 	######
+sleep 1
+
+
+########    Install Docker-Compose 	########
 echo -n $(sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null 2>&1)
 echo -ne '|################    |(80%)\r'
 sleep 1 
 echo -n $(sudo chmod +x /usr/local/bin/docker-compose > /dev/null 2>&1)
 echo -ne '|##################  |(90%)\r'
-sleep 1 
+sleep 1
+
+########   Test de version Docker-Compose	########
 echo -n $(docker-compose --version)
 echo -ne '|####################|(100%)\r'
 echo -ne '\n'
