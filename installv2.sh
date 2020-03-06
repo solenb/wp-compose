709c0784d8fb9027045682412ea60bcceb0c603a
Votre Nom <Vous@exemple.com>
2020-03-06 10:45:57 +0100

    install v2.2

--- installv2.sh
+++ installv2.sh
@@ -1,3 +1,5 @@
+echo -n ("Lancement du script d'installation de Docker et Docker-Compose")
+
 apt-get update
 apt-get install -y \
 linux-image-extra-$(uname -r) \
@@ -24,4 +26,5 @@ apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+#echo -n
 docker-compose --version
