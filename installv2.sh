a71fa282d6c17e97b16e4c8cd8a9bc324585fbbd
Votre Nom <Vous@exemple.com>
2020-03-07 13:39:52 +0100

    install v2.13

--- installv2.sh
+++ installv2.sh
@@ -76,9 +76,9 @@ if [[ $(echo $quest) = "y" ]]; then
 	echo -e "Mise en production du containeur\n"
 	docker-compose up -d
 elif [[ $(echo $quest) = "n" ]]; then 
-	echo -e "Vous pourrez mettre le containeur (ou utiliser un autre fichier yaml), la mise en production a été stoppé\n"
+	echo -e "Vous pourrez mettre le containeur (ou utiliser un autre fichier yaml), en production en effectuant la commande : \n\t docker-compose up -d"
 else
-	echo -e "La mise en production a été stoppé, veuillez remplir votre choix [y-n], en relançant le script\n"
+	echo -e "La mise en production a été stoppé, veuillez remplir correctement votre choix [y-n], en relançant le script\n"
 fi
 
 
